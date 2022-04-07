c88f5ce9aa1a04be0ab3531dc735265e4673e1b6
Salma Elansary <elansary@magazino.eu>
2022-04-07 17:42:46 +0200

    Adpat scripts to problem and LNS changes

--- parallel_lns_greedy.py
+++ parallel_lns_greedy.py
@@ -1,8 +1,7 @@
 import multiprocessing as mp
+import time
 from copy import deepcopy
 from typing import List, Optional
-
-from gurobipy import GRB
 
 from core import LNS, Compartment, Item, Problem, Solution, Vehicle
 
@@ -10,14 +9,14 @@ from core import LNS, Compartment, Item, Problem, Solution, Vehicle
 def generate_initial_solutions(
     problem: Problem,
     path: List[int],
+    # current state of the stacks as a mapping of stored item vertices
     stacks: List[List[Optional[int]]],
+    # assignment as a partial solution
     stack_assignment: List[List[Optional[int]]],
 ):
-    N, V, P, D, C, M = problem.problem_vars()
+    N, V, P, D, C, M = problem.problem_data()
     items = problem.items
-    stack_capacities = [c.capacity for c in problem.vehicle.compartments]
-
-    # print(path)
+    compartments = problem.vehicle.compartments
 
     if len(path) == len(V):
         yield list(path), deepcopy(stack_assignment)
@@ -33,17 +32,28 @@ def generate_initial_solutions(
         next_possible = list(next_possible)
         next_possible = sorted(next_possible, key=lambda v: C[curr][v])
 
-        collected_demands = [sum(items[i].length for i in stack) for stack in stacks]
+        collected_demands = [
+            sum(compartments[idx].demand_for_item(items[i]) for i in stack)
+            for idx, stack in enumerate(stacks)
+        ]
         for v in next_possible:
             is_pickup = v <= N
+            pick_v = v if is_pickup else v - N
+            item = items[pick_v]
 
-            if is_pickup:
-                v_demand = items[v].length
-            else:
-                v_demand = -items[v - N].length
+            compatible_stacks = [
+                (i, comp)
+                for i, comp in enumerate(compartments)
+                if comp.is_item_compatible(item)
+            ]
+            for i, comp in compatible_stacks:
+                v_demand = comp.demand_for_item(item)
 
-            for i in M:
-                if is_pickup and collected_demands[i] + v_demand <= stack_capacities[i]:
+                if not is_pickup:
+                    # delivery demand
+                    v_demand = -v_demand
+
+                if is_pickup and collected_demands[i] + v_demand <= comp.capacity:
                     path.append(v)
                     stacks[i].append(v)
                     stack_assignment[i].append(v)
@@ -56,7 +66,7 @@ def generate_initial_solutions(
                     stack_assignment[i].pop()
 
                 if not is_pickup and stacks[i]:
-                    pick_v = v - N
+                    # delivery vertex, remove item mapping from top of stack
                     if pick_v == stacks[i][-1]:
                         path.append(v)
                         stacks[i].pop()
@@ -69,6 +79,7 @@ def generate_initial_solutions(
 
 
 def test():
+    # High level Parallelism (Multiple LNSs running in parallel, but no)
     nb_items = 15
     nb_compartments = 5
     items = [Item(400) for _ in range(nb_items)]
@@ -76,9 +87,6 @@ def test():
 
     # Define the problem
     pdp = Problem(items, vehicle=vehicle)
-    pdp.create_model()
-    pdp.apply_constraints()
-    pdp.set_model_objective()
 
     gen = generate_initial_solutions(
         pdp,
@@ -88,27 +96,38 @@ def test():
     )
 
     initial_solutions = []
-    for i in range(6):
+    for i in range(mp.cpu_count()):
         path, stack_assignment = next(gen)
-        initial_solutions.append(Solution(pdp.items, path, stack_assignment))
+        initial_solutions.append(Solution(pdp.items, path, stack_assignment, vehicle))
+    initial_results = [pdp.evaluate_solution(sol) for sol in initial_solutions]
 
+    def search(solver, qu):
+        solution = solver.search()
+        qu.put(pdp.evaluate_solution(solution))
+
+    s_time = time.time()
+    q = mp.Queue()
     processes = []
     for initial_solution in initial_solutions:
         # start a new LNS process with the new solution
         lns_timelimit = 15
         lns_solver = LNS(
             pdp,
-            max_iteration=1000,
+            max_iterations=1000,
             time_limit=lns_timelimit,
             initial_solution=initial_solution,
         )
-        p = mp.Process(target=lns_solver.search_local_solution)
+        p = mp.Process(target=search, args=(lns_solver, q))
         processes.append(p)
         p.start()
 
+    results = [q.get() for p in processes]
     for p in processes:
         p.join()
 
+    print(f"Inital solutions objective {initial_results}")
+    print(f"Objectives of different LNSs {results} in {time.time() - s_time}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     test()
